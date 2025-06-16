d06e6cf94d64fe04ae2d68445c7974e2c9930ffb
Cong Lyu <15602214230@163.com>
2025-06-16 17:45:11 +0930

    separate the products list from Amazon_products.js.

--- Scripts/Amazon_products.js
+++ Scripts/Amazon_products.js
@@ -1,46 +1,4 @@
-const products = [{
-  productId : 'sa21dk12ks1o',
-  productName : 'Black and Gray Athletic',
-  productRating : 45,
-  productScore : 87,
-  productPriceCents : 1090,
-  productImageName : 'athletic-cotton-socks-6-pairs'
-},{
-  productId : 'sa21sa12ssao',
-  productName : 'Intermediate Size Basketball',
-  productRating : 40,
-  productScore : 127,
-  productPriceCents : 2095,
-  productImageName : 'intermediate-composite-basketball'
-},{
-  productId : 'sa2ewwqao',
-  productName : 'Adults Plain Cotton T-Shirt - 2 Pack',
-  productRating : 45,
-  productScore : 56,
-  productPriceCents : 799,
-  productImageName : 'adults-plain-cotton-tshirt-2-pack-teal'
-},{
-  productId : 'sadkiuao',
-  productName : '2 Slot Toaster - Black',
-  productRating : 50,
-  productScore : 2197,
-  productPriceCents : 1899,
-  productImageName : 'black-2-slot-toaster'
-},{
-  productId : 'sad447475o',
-  productName : '6 Piece White Dinner Plate Set',
-  productRating : 40,
-  productScore : 37,
-  productPriceCents : 2067,
-  productImageName : '6-piece-white-dinner-plate-set'
-},{
-  productId : 'sadqw2rerao',
-  productName : '6-Piece Nonstick, Carbon Steel Oven',
-  productRating : 45,
-  productScore : 175,
-  productPriceCents : 3499,
-  productImageName : '6-piece-non-stick-baking-set'
-}];
+import {products} from './products.js'
 
 let productsContentHTML = ``;
 const productContent = document.querySelector('.products-content');
@@ -81,6 +39,7 @@ products.forEach((item, index) => {
     </button>
   </div>`;
 })
+
 productContent.innerHTML = productsContentHTML;
 const productAmount = document.querySelector('.product-number');
 const addToCartButtons = document.querySelectorAll('.Add-to-Cart');
