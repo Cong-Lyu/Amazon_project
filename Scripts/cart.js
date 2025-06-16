f920007c6ff5b2ad71fc00c25ac4b11b48dbfc17
Cong Lyu <15602214230@163.com>
2025-06-16 17:50:44 +0930

    Make checkoutContent with cart with JScode AND separate the cart into a cart.js file.

---
+++ Scripts/cart.js
@@ -0,0 +1,9 @@
+export const cart = [{
+  productId: 'sa21dk12ks1o',
+  productQuantity: 6,
+  productDropDate: '2025-06-05'
+},{
+  productId: 'sa21sa12ssao',
+  productQuantity: 2,
+  productDropDate: '2025-06-15'
+}];
