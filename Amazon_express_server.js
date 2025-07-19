f47568fc141ee022b78ca267102a1e5b85b2d441
Cong Lyu <15602214230@163.com>
2025-07-19 17:17:40 +0930

    Install dotenv package and hide sensitive parameters into .env file.

--- Amazon_express_server.js
+++ Amazon_express_server.js
@@ -1,7 +1,25 @@
 const express = require('express');
 const amazonServer = express();
+const mysql2 = require('mysql2');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const pool = mysql2.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
+}).promise()
 
 amazonServer.use(express.static('./public'));
+amazonServer.use(express.json());
+
+amazonServer.post('/login', async (req, res) => {
+  const queryBody = 'INSERT INTO users (userPassword, userEmail, lastLogin) VALUES(?, ?, NOW())';
+  const result = await pool.query(queryBody, [req.body.userPassword, req.body.userEmail])
+  res.status(200).send(result);
+})
+
 
 amazonServer.listen(5000, () => {
   console.log('Now Amazon server is created and listening requests from port 5000......');
